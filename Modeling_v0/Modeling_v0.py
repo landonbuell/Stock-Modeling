663c0b13e9db5f12b953c73506a6412b82ef25ce
landonbuell <lhb1007@wildcats.unh.edu>
2020-03-31 13:24:32 -0400

    Initial Commit
    - Created Model_v0 Project

---
+++ Modeling_v0/Modeling_v0.py
@@ -0,0 +1,34 @@
+"""
+Landon Buell
+Modeling v0
+Main Executable
+31 March 2020
+"""
+
+            #### IMPORTS ####
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import yfinance as yf
+
+if __name__ == '__main__':
+
+    TSLA = yf.download(tickers=['TSLA'],period='max')
+
+    print(TSLA.head(20))
+    print(TSLA.columns)
+
+    x = TSLA.index
+    y1 = TSLA['Open']
+    y2 = TSLA['Close']
+
+    plt.plot(x,y1,color='blue',label='Open')
+    plt.plot(x,y2,color='orange',label='Close')
+    plt.grid()
+    plt.legend()
+    plt.show()
+    
+
+    
