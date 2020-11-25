7860abc9546371498f395055c5f3b74c4108cf7e
landonbuell <lhb1007@wildcats.unh.edu>
2020-11-25 18:04:27 -0500

    Progress with IronPython

---
+++ CSharpAndPython/CSharpAndPython/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using IronPython.Hosting;
+
+namespace CSharpAndPython
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is C# Speaking!");
+            Console.WriteLine("Waiting on Python...");
+
+            string pyScriptPath = @"C:\Users\Landon\Documents\GitHub\Stock-Modeling\CSharpAndPython\PythonBackend\PythonBackend.py";
+
+            var PythonEngine = Python.CreateEngine();
+            var Scope = PythonEngine.CreateScope();
+            var Source = PythonEngine.CreateScriptSourceFromFile(pyScriptPath);
+
+            Console.WriteLine("This is C# Again!");           
+        }
+    }
+}
---
+++ CSharpAndPython/PythonBackend/PythonBackend.py
@@ -0,0 +1,20 @@
+
+def Exponent(a,b):
+    """ Raise a to the power of b """
+    try:
+        return a**b
+    except:
+        raise ValueError()
+
+if __name__ == "__main__":
+
+    print("Hello, This is Python!")
+
+    #a = sys.argv[1]
+    #b = sys.argv[2]
+
+    a,b = 2 , 10
+    print(str(a)+"^"+str(b)+"=",Exponent(a,b))
+
+    print("Python is done, goodbye!")
+    
